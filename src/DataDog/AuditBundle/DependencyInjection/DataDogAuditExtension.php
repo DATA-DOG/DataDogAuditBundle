3d12967e0dc804169e7fc53e00955759c51cd03b
Nate Wiebe <nate@northern.co>
2022-01-10 13:59:48 -0500

    Remove yaml dependency

--- src/DataDog/AuditBundle/DependencyInjection/DataDogAuditExtension.php
+++ src/DataDog/AuditBundle/DependencyInjection/DataDogAuditExtension.php
@@ -4,8 +4,8 @@ namespace DataDog\AuditBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 
 class DataDogAuditExtension extends Extension
 {
@@ -17,8 +17,8 @@ class DataDogAuditExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
 
         $auditSubscriber = $container->getDefinition('datadog.event_subscriber.audit');
 

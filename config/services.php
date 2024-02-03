f4dffb3bfaf31911d4122812e8d76b663f570440
Georgi Kolev <georgi.kolev@geniussports.com>
2024-02-03 13:55:03 +0200

    command refactoring

--- config/services.php
+++ config/services.php
@@ -2,6 +2,7 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
+use DataDog\AuditBundle\Command\AuditLogDeleteOldLogsCommand;
 use DataDog\AuditBundle\EventListener\AuditListener;
 use Doctrine\ORM\Events;
 use Symfony\Component\DependencyInjection\Reference;
@@ -14,6 +15,10 @@ return static function (ContainerConfigurator $container) {
         ->set('datadog.event_listener.audit', AuditListener::class)->private()
         ->arg(0, new Reference(TokenStorageInterface::class))
         ->tag('doctrine.event_listener', ['event' => Events::onFlush,])
+    ;
+
+    $services->set('datadog.command.delete_old_logs', AuditLogDeleteOldLogsCommand::class)
+        ->tag('console.command')
     ;
     // @formatter:on
 };

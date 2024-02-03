1a028e00b6d8e73bc31e88b0fe9c133e7b9b03cb
Georgi Kolev <georgi.kolev@geniussports.com>
2024-02-03 12:55:09 +0200

    services refactoring

---
+++ config/services.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use DataDog\AuditBundle\EventListener\AuditListener;
+use Doctrine\ORM\Events;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+return static function (ContainerConfigurator $container) {
+    // @formatter:off
+    $services = $container->services();
+    $services
+        ->set('datadog.event_listener.audit', AuditListener::class)->private()
+        ->arg(0, new Reference(TokenStorageInterface::class))
+        ->tag('doctrine.event_listener', ['event' => Events::onFlush,])
+    ;
+    // @formatter:on
+};
--- src/DependencyInjection/DataDogAuditExtension.php
+++ src/DependencyInjection/DataDogAuditExtension.php
@@ -2,7 +2,6 @@
 
 namespace DataDog\AuditBundle\DependencyInjection;
 
-use DataDog\AuditBundle\EventListener\AuditListener;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
@@ -12,13 +11,13 @@ class DataDogAuditExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         $loader->load('services.php');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $auditListener = $container->getDefinition(AuditListener::class);
+        $auditListener = $container->getDefinition('datadog.event_listener.audit');
 
         if (isset($config['audited_entities']) && !empty($config['audited_entities'])) {
             $auditListener->addMethodCall('addAuditedEntities', array($config['audited_entities']));

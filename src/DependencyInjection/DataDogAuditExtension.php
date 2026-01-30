b4fc89ea0fad755989c9c5915ca464d9cb408a80
Georgi <4854116+indjeto@users.noreply.github.com>
2026-01-30 14:53:32 +0200

    Php 8.4 deprecations fix

--- src/DependencyInjection/DataDogAuditExtension.php
+++ src/DependencyInjection/DataDogAuditExtension.php
@@ -5,7 +5,7 @@ namespace DataDog\AuditBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class DataDogAuditExtension extends Extension
 {
